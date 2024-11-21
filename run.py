b37bc68ec8be4fa324ba4c1a51d31ab852891ce7
Sumiran Jaiswal <sumi0309@umd.edu>
2024-11-21 13:54:57 -0500

    initialize feature 3 to extract reopened_issues

--- run.py
+++ run.py
@@ -9,6 +9,7 @@ import argparse
 
 import config
 from example_analysis import ExampleAnalysis
+from issue_lifecycle_analysis import IssueLifecycleAnalysis
 
 
 def parse_args():
@@ -52,6 +53,6 @@ elif args.feature == 1:
 elif args.feature == 2:
     pass # TODO call second analysis
 elif args.feature == 3:
-    pass # TODO call third analysis
+    IssueLifecycleAnalysis().run()
 else:
     print('Need to specify which feature to run with --feature flag.')
